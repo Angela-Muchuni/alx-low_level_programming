ad4c4c6f81ce5b8105840f21c2aef6aa69e8ddfe
Angela Muchuni <charlottemuthoni925@gmail.com>
2023-02-17 06:36:16 -0800

    TASK11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ int main(void)
 			for (ones = '0'; ones <= '9'; ones++)
 			{
 				if (!((ones == tens) || (tens == hundreds) ||
-						(tens > ones) || (hundreds > tens))) /*elminates repetition*/
+							(tens > ones) || (hundreds > tens))) /*elminates repetition*/
 				{
 					putchar(hundreds);
 					putchar(tens);
