bc16f6e86a0625403895a6b2a4e25d4d953e3f2a
AngieKinga <musanga1911@gmail.com>
2023-03-27 01:26:13 -0700

    TASK5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,8 +8,7 @@
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
-{
-	int i = 0;
+{int i = 0;
 
 	while (s[i])
 	{
@@ -25,12 +24,10 @@ int is_digit(char *s)
  * Return: the length of the string
  */
 int _strlen(char *s)
-{
-	int i = 0;
+{int i = 0;
 
 	while (s[i] != '\0')
-	{
-		i++;
+	{i++;
 	}
 	return (i);
 }
@@ -38,8 +35,7 @@ int _strlen(char *s)
  * errors - handles errors for main
  */
 void errors(void)
-{
-	printf("Error\n");
+{printf("Error\n");
 	exit(98);
 }
 /**
@@ -49,33 +45,23 @@ void errors(void)
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
-{
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+{char *s1, *s2; int len1, len2, len, i, carry, digit1, digit2,
+	    *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
+	len1 = _strlen(s1); len2 = _strlen(s2); len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-
+	{digit1 = s1[len1] - '0'; carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
+		{digit2 = s2[len2] - '0'; carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10; carry /= 10;
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
@@ -89,7 +75,6 @@ int main(int argc, char *argv[])
 	}
 	if (!a)
 		_putchar('0');
-	_putchar('\n');
-	free(result);
+	_putchar('\n'); free(result);
 	return (0);
 }
